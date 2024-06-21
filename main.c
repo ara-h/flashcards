0fcb14b6ce967eb33d30225fd68451527bb21e8e
Ara Hanissian <ajhanissianjr@icloud.com>
2024-06-21 18:23:08 -0500

    Implement a function to trim whitespace and copy n characters(lrtrimncpy). Use that function to fix print_card_node.

--- main.c
+++ main.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define FRONT_MAX 512
 #define BACK_MAX 512
-
 
 typedef struct {
 	char front[FRONT_MAX];
@@ -25,6 +23,7 @@ typedef struct {
 	Card_node* head;	
 } Deck;
 
+
 void card_prompt(Card card);
 
 size_t card_ll_length(Card_node* head);
@@ -33,13 +32,15 @@ void free_card_node(Card_node* head);
 
 void free_deck(Deck* deck);
 
+void lrtrimncpy(char* dst, char* src, size_t n);
+
 void print_card_node(Card_node* node);
 
 Deck* load_deck(FILE* f);
 
 int main(void) {
 	// load from text file
-	FILE* fp = fopen("deck1.txt", "r");
+	FILE* fp = fopen("deck0.txt", "r");
 	if (fp == NULL) {
 		printf("The file was not opened. The program will "
 				"now exit.\n");
@@ -49,6 +50,7 @@ int main(void) {
 	fclose(fp);
 	printf("Loaded deck.\n");
 
+	print_card_node(current_deck->head);
 
 	free_deck(current_deck);
 
@@ -96,14 +98,34 @@ void free_deck(Deck* deck) {
 }
 
 
+void lrtrimncpy(char* dst, char* src, size_t n) {
+  // Trim whitespace and copy n characters
+  // dst must have room for n characters and a null terminator
+  char* srcStart = src;
+  while(*srcStart == ' ') srcStart++;
+  char* srcEnd = srcStart;
+  while(*srcEnd != '\0' && *srcEnd != '\n' && *srcEnd != '\r') srcEnd++;
+  while(*srcEnd == ' ') srcEnd--;
+  int b = (srcEnd - srcStart);
+  int i = 0;
+  while(i < n && i < b) {
+    *(dst + i) = *(srcStart + i);
+    i++;
+  }
+  *(dst + i) = '\0';
+  return;
+}
+
+
 void print_card_node(Card_node* node) {
-	char fr[12];
+	Card_node* temp = node;
+  char fr[12];
 	char ba[12];
-	while (node) {
-		strncpy(fr, node->data->front, 11);
-		strncpy(ba, node->data->back, 11);
+	while (temp != NULL) {
+		lrtrimncpy(fr, temp->data->front, 11);
+		lrtrimncpy(ba, temp->data->back, 11);
 		printf("< %s...| %s... >\n", fr, ba);
-		node = node->next;
+		temp = temp->next;
 	}
 	return;
 }
@@ -115,13 +137,11 @@ Deck* load_deck(FILE* f) {
 	if (fgets(deck_name, sizeof(deck_name), f) == NULL) return NULL;
 	Deck* d = malloc(sizeof(Deck));
 	strcpy(d->name, deck_name);
-	printf("deck name: %s\n",d->name); // testing
+	// printf("deck name: %s\n",d->name); // testing
 	d->deck_size = 0;
 	d->head = NULL;
 	
-	// get front of first card
 	char buff[FRONT_MAX];
-	// if (fgets(buff, sizeof(buff), f) == NULL) return d;
 	// make a new card linked list
 	Card_node* head = malloc(sizeof(Card_node));
 	head->next = NULL;
@@ -132,17 +152,21 @@ Deck* load_deck(FILE* f) {
 	int front = 1;
 	while (fgets(buff, sizeof(buff), f) != NULL) {
 		if (front) {
-			new_card = malloc(sizeof(Card));
+      if (size > 0) {
+        // Create and advance node here
+        node->next = malloc(sizeof(Card_node));
+        node = node->next;
+        node->next = NULL;
+      }
+      new_card = malloc(sizeof(Card));
 			strcpy(new_card->front, buff);
-			printf("Read card front: %s\n", (new_card->front));
+			// printf("Read card front: %s\n", (new_card->front));
 			front = 0;
 		} else {
 			strcpy(new_card->back, buff);
-			printf("Read card back: %s\n", (new_card->back));
+			// printf("Read card back: %s\n", (new_card->back));
+      // We are putting the data in here, but not advancing to the next node
 			node->data = new_card;
-			node->next = malloc(sizeof(Card_node));
-			node = node->next;
-			node->next = NULL;
 			size++;
 			front = 1;
 		}
@@ -156,7 +180,8 @@ Deck* load_deck(FILE* f) {
 		} else {
 			// front was read but no back
 			// insert message as back of card
-			char blank_msg[] = "flashcards: the back of this card wasn't found";
+			char blank_msg[] = "flashcards: the back of this card wasn't found\n";
+			// printf("Read card back: %s\n", blank_msg);
 			strcpy(new_card->back, blank_msg);
 			node->data = new_card;
 			size++;
