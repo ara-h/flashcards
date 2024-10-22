7958827f4e780f2b097dcb72d1ed204cc10b8854
Ara Hanissian <ajhanissianjr@icloud.com>
2024-10-21 21:48:57 -0500

    Menu userptr and bilding

--- main.c
+++ main.c
@@ -7,78 +7,78 @@
 void print_window_title(WINDOW *win, int starty, int startx, int width,
                         char *str, int str_len);
 
-int main(int argc, char *argv[]) {
+WINDOW *make_main_menu_win(MENU *mm, WINDOW *win);
 
+void placeholder_func(char *name, WINDOW *win);
+
+int main(int argc, char *argv[]) {
+  /*
+  char *choices[] = {"Study deck                                 s",
+                     "List decks                                 l",
+                     "Edit decks                                 e",
+                     "Load new deck from file                    o",
+                     "Delete deck                                d",
+                     "Exit                                       q"};
+  */
   char *choices[] = {"Study deck",  "List decks",
                      "Edit decks",  "Load new deck from file",
                      "Delete deck", "Exit"};
-
   int n_choices = sizeof(choices) / sizeof(choices[0]);
-  // n_choices must be less than 99
-  char **choice_indexes = (char **)malloc(n_choices * sizeof(char *));
-  int i;
-  for (i = 0; i < n_choices; i++) {
-    choice_indexes[i] = (char *)malloc(4 * sizeof(char));
-  }
-  for (i = 0; i < n_choices - 1; i++) {
-    sprintf(choice_indexes[i], "%d.", i + 1);
-  }
-  sprintf(choice_indexes[n_choices - 1], "0.");
 
+  // Initialize curses
   initscr();
-
-  start_color();
+  use_default_colors();
 
   cbreak();
   noecho();
   keypad(stdscr, TRUE);
 
+  // Set menu items
   ITEM **mm_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+  int i;
   for (i = 0; i < n_choices; ++i) {
-    mm_items[i] = new_item(choice_indexes[i], choices[i]);
+    mm_items[i] = new_item(choices[i], NULL);
+    set_item_userptr(mm_items[i], placeholder_func);
   }
   mm_items[n_choices] = (ITEM *)NULL;
 
+  // Make main menu
   MENU *main_menu = new_menu((ITEM **)mm_items);
+  // Set menu to not show description
+  menu_opts_off(main_menu, O_SHOWDESC);
+  // Make main menu window
+  WINDOW *main_menu_win = make_main_menu_win(main_menu, NULL);
 
-  WINDOW *main_menu_win = newwin(10, 40, 4, 4);
-  keypad(main_menu_win, TRUE);
-
-  set_menu_win(main_menu, main_menu_win);
-  set_menu_sub(main_menu, derwin(main_menu_win, 6, 38, 3, 1));
-
-  set_menu_mark(main_menu, " * ");
-
-  box(main_menu_win, 0, 0);
-  char title[] = "FLASHCARDS";
-  int title_len = sizeof(title) / sizeof(title[0]);
-  print_window_title(main_menu_win, 1, 0, 40, title, title_len);
-  mvwaddch(main_menu_win, 2, 0, ACS_LTEE);
-  mvwhline(main_menu_win, 2, 1, ACS_HLINE, 38);
-  mvwaddch(main_menu_win, 2, 39, ACS_RTEE);
-  mvprintw(LINES - 3, 0, "Press Up and Down arrow keys to navigate,");
-  mvprintw(LINES - 2, 0, "<ENTER> to select, or q to exit");
-  refresh();
-
-  post_menu(main_menu);
-  wrefresh(main_menu_win);
-
+  ITEM *cur;
+  void (*p)(char *, WINDOW *);
   int c;
   while ((c = wgetch(main_menu_win)) != 'q') {
     switch (c) {
+    case 'j':
     case KEY_DOWN:
       menu_driver(main_menu, REQ_DOWN_ITEM);
       break;
+    case 'k':
     case KEY_UP:
       menu_driver(main_menu, REQ_UP_ITEM);
       break;
+    case 10: // 'Enter'
+      cur = current_item(main_menu);
+      p = item_userptr(cur);
+      p((char *)item_name(cur), main_menu_win);
+      pos_menu_cursor(main_menu);
+      break;
+      // Do something on pressing Enter
     }
+
+    // but if wgetch returns the window resize char, then move the window to new
+    // center
     wrefresh(main_menu_win);
   }
 
+  // Unpost menu and free
   unpost_menu(main_menu);
   free_menu(main_menu);
-  free(choice_indexes);
   for (i = 0; i < n_choices; i++)
     free_item(mm_items[i]);
   endwin();
@@ -88,18 +88,56 @@ int main(int argc, char *argv[]) {
 void print_window_title(WINDOW *win, int starty, int startx, int width,
                         char *str, int str_len) {
   int x, y;
-  if (win == NULL)
+  if (win == NULL) {
     win = stdscr;
+  }
   getyx(win, y, x);
-  if (startx != 0)
+  if (startx != 0) {
     x = startx;
-  if (starty != 0)
+  }
+  if (starty != 0) {
     y = starty;
-  if (width == 0)
+  }
+  if (width == 0) {
     width = 80;
+  }
 
   int temp = (width - str_len) / 2;
   x = startx + temp;
   mvwprintw(win, y, x, "%s", str);
   refresh();
+}
+
+WINDOW *make_main_menu_win(MENU *mm, WINDOW *win) {
+  endwin();
+
+  if (win == NULL) {
+    win = stdscr;
+  }
+
+  int start_col = (getmaxx(win) / 2) - 24; // maxx betta be >= 48
+  int start_row = (getmaxy(win) / 2) - 5;
+
+  WINDOW *main_menu_win = newwin(10, 48, start_row, start_col);
+  keypad(main_menu_win, TRUE);
+
+  set_menu_win(mm, main_menu_win);
+  WINDOW *main_menu_derwin = derwin(main_menu_win, 8, 46, 1, 1);
+  set_menu_sub(mm, main_menu_derwin);
+
+  box(main_menu_win, 0, 0);
+  char title[] = "FLASHCARDS";
+  int title_len = sizeof(title) / sizeof(title[0]);
+  print_window_title(main_menu_win, 0, 1, 48, title, title_len);
+  refresh();
+  post_menu(mm);
+  wrefresh(main_menu_win);
+  // TODO: does this leak memory? the WINDOWs?
+  return main_menu_win;
+}
+
+void placeholder_func(char *name, WINDOW *win) {
+  wmove(win, 8, 1);
+  clrtoeol(); // This doesn't really clear to end of line
+  mvwprintw(win, 8, 1, "Item selected is : %s", name);
 }
